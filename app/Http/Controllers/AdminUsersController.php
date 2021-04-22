793cdcdcda543403739fe0ffd40d1c3866c2027a
unknown <plg.cardano@gmail.com>
2021-04-23 07:51:42 +0800

    creating users with photos working with Photo model

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Role;
 use App\User;
+use App\Photo;
 use Illuminate\Http\Request;
 use App\Http\Requests\UsersRequest;
 
@@ -44,14 +45,24 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        // return $request->all();
-        User::create($request->all());
+        $input = $request->all();
 
-        if('photo_id'){
+        if($file = $request->file('photo_id')){
 
-            $request('photo_id')->store('images');
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
 
         }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
 
         return redirect()->route('users.index');
 
--- app/User.php
+++ app/User.php
@@ -43,5 +43,11 @@ class User extends Authenticatable
 
     }
 
+    public function photo(){
+
+        return $this->belongsTo('App\Photo');
+
+    }
+
 
 }
